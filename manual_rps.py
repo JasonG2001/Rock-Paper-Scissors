aa6578939fba760926fc4543524e25cc89a5885a
Jason <xiaohemc2001@gmail.com>
2022-07-14 23:43:01 +0100

    Adding the code to identify the winner

--- manual_rps.py
+++ manual_rps.py
@@ -1,13 +1,24 @@
 import random
 
-options = ["Rock", "Paper", "Scissors"]
+options = ["Rock", "Paper", "Scissors"] #Options for the computer to choose from
+computer_choice = random.choice(options)
 
 def get_computer_choice():
-    print(f"Computer plays {random.choice(options)}") 
+    print(f"Computer plays {computer_choice}") #This gives the computer option
 
-choice = input("Enter rock, paper or scissors: ")
-def get_user_choice(choice):
-    print(choice.capitalize())
+user_choice = input("Enter rock, paper or scissors: ")
+def get_user_choice():
+    print(f"You play {user_choice.capitalize()}") #This gives the user option
 
-get_user_choice(choice)
+get_user_choice()
 get_computer_choice()
+
+def get_winner():
+    if user_choice.capitalize() == computer_choice:
+        print("Draw")
+    elif (user_choice.capitalize() == "Rock" and computer_choice == "Scissors") or (user_choice.capitalize() == "Scissors" and computer_choice == "Paper") or (user_choice.capitalize() == "Paper" and computer_choice == "Rock"):
+        print("You win")
+    else:
+        print("You lose")
+
+get_winner()
