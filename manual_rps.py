924aedeed563d37189f581eff77c62b8c03cb417
Jason <xiaohemc2001@gmail.com>
2022-07-15 15:38:23 +0100

    Finishing module 3

--- manual_rps.py
+++ manual_rps.py
@@ -1,24 +1,26 @@
 import random
 
-options = ["Rock", "Paper", "Scissors"] #Options for the computer to choose from
-computer_choice = random.choice(options)
-
 def get_computer_choice():
-    print(f"Computer plays {computer_choice}") #This gives the computer option
+    OPTIONS = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(OPTIONS)
+    return computer_choice # This gives the computer option
 
-user_choice = input("Enter rock, paper or scissors: ")
 def get_user_choice():
-    print(f"You play {user_choice.capitalize()}") #This gives the user option
+    user_choice = input("Enter rock, paper or scissors: ").capitalize()
+    return user_choice 
 
-get_user_choice()
-get_computer_choice()
-
-def get_winner():
-    if user_choice.capitalize() == computer_choice:
-        print("Draw")
-    elif (user_choice.capitalize() == "Rock" and computer_choice == "Scissors") or (user_choice.capitalize() == "Scissors" and computer_choice == "Paper") or (user_choice.capitalize() == "Paper" and computer_choice == "Rock"):
-        print("You win")
+def get_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "draw"
+    elif (user_choice == "Rock" and computer_choice == "Scissors") or (user_choice == "Scissors" and computer_choice == "Paper") or (user_choice == "Paper" and computer_choice == "Rock"):
+        return "user"
     else:
-        print("You lose")
+        return "computer"
 
-get_winner()
+def play():
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    get_winner(user_choice, computer_choice)
+
+if __name__ == "__main__":
+    play()
