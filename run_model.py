2b70797e2cf27d75698ad12e81116cc97e312910
Jason <xiaohemc2001@gmail.com>
2022-07-17 00:15:29 +0100

    Making the 5 second timer

--- run_model.py
+++ run_model.py
@@ -1,9 +1,11 @@
 import cv2
+import time
 from keras.models import load_model
 import numpy as np
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
 
 while True: 
     ret, frame = cap.read()
@@ -17,8 +19,9 @@ while True:
     print(prediction)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-            
+
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
+
