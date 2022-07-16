b69a166a71751647efbdcb578edc6f337853889a
Jason <xiaohemc2001@gmail.com>
2022-07-16 10:14:54 +0100

    First commit of prediction

---
+++ camera_rps.py
@@ -0,0 +1,6 @@
+C:/Users/xiaoh/miniconda3/envs/RPS/python.exe c:/Users/xiaoh/OneDrive/Documents/AICore/Rock-Paper-Scissors/camera_rps.py
+sfsfC:/Users/xiaoh/miniconda3/envs/RPS/python.exe c:/Users/xiaoh/OneDrive/Documents/AICore/Rock-Paper-Scissors/camera_rps.py
+C:/Users/xiaoh/miniconda3/envs/RPS/python.exe c:/Users/xiaoh/OneDrive/Documents/AICore/Rock-Paper-Scissors/camera_rps.py
+C:/Users/xiaoh/miniconda3/envs/RPS/python.exe c:/Users/xiaoh/OneDrive/Documents/AICore/Rock-Paper-Scissors/camera_rps.py
+C:/Users/xiaoh/miniconda3/envs/RPS/python.exe c:/Users/xiaoh/OneDrive/Documents/AICore/Rock-Paper-Scissors/camera_rps.py
+
