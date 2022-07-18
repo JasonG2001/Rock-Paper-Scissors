c43039633d76bb7746d4a111bc6a165648512c07
Jason <xiaohemc2001@gmail.com>
2022-07-18 18:31:51 +0100

    only missing the count to 3 to win

--- camera_rps copy.py
+++ camera_rps copy.py
@@ -4,34 +4,40 @@ import random
 computer_wins = 0
 user_wins = 0
 
-while computer_wins < 3 and user_wins < 3:
+def final_winner(computer_wins, user_wins):
+    while computer_wins < 3 and user_wins < 3:
 
-    def get_computer_choice():
-        OPTIONS = ["Rock", "Paper", "Scissors"]
-        computer_choice = random.choice(OPTIONS)
-        return computer_choice 
+        def get_computer_choice():
+            OPTIONS = ["Rock", "Paper", "Scissors"]
+            computer_choice = random.choice(OPTIONS)
+            return computer_choice 
 
-    def get_prediction():
-        return run_model.run() 
+        def get_prediction():
+            return run_model.run() 
 
-    def get_winner(prediction, computer_choice):
-        if prediction == computer_choice:
-            print("draw")
-            return "draw"
-        elif (prediction == "Rock" and computer_choice == "Scissors") or (prediction == "Scissors" and computer_choice == "Paper") or (prediction == "Paper" and computer_choice == "Rock"):
-            user_wins += 1
-            print("you win")
-            return "user"
-        else:
-            computer_wins += 1
-            print("computer wins")
-            return "computer"
-
-
-    def play():
-        prediction = get_prediction()
-        computer_choice = get_computer_choice()
-        get_winner(prediction, computer_choice)
+        def get_winner(prediction, computer_choice):
+            if prediction == computer_choice:
+                print("draw")
+                return "draw"
+            elif (prediction == "Rock" and computer_choice == "Scissors") or (prediction == "Scissors" and computer_choice == "Paper") or (prediction == "Paper" and computer_choice == "Rock"):
+                print("you win")
+                return "user"
+            else:
+                print("computer wins")
+                return "computer"
 
 
-    play()
+        def play():
+            prediction = get_prediction()
+            computer_choice = get_computer_choice()
+            get_winner(prediction, computer_choice)
+
+        if __name__ == "__main__":
+            play()
+
+    if computer_wins > user_wins:
+        print("computer wins overall")
+    else:
+        print("user wins overall")
+
+final_winner(computer_wins, user_wins)
