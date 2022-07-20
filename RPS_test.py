f950ba7e7e23506fa3e20511c8017d36cec2a619
Jason <xiaohemc2001@gmail.com>
2022-07-20 14:24:40 +0100

    Using OOP to make the RPS game

---
+++ RPS_test.py
@@ -0,0 +1,102 @@
+import random
+import cv2
+import time
+from keras.models import load_model
+import numpy as np
+
+class RockPaperScissors:
+
+    def __init__(self):
+        
+        self.user_choices = ["rock", "paper", "scissors", "nothing"]
+        self.computer_choices = ["rock", "paper", "scissors"]
+        self.user_wins = 0
+        self.computer_wins = 0
+        self.points_to_win = 3
+
+    
+    def get_computer_choice(self):
+
+        self.computer_choice = random.choice(self.computer_choices)
+        print(f"Computer plays {self.computer_choice}")
+        return self.computer_choice
+
+    
+    def get_user_choice(self):
+
+        model = load_model('keras_model.h5')
+        cap = cv2.VideoCapture(0)
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        start_time = time.time()
+        TIME_LIMIT = 4
+        while time.time() < start_time + TIME_LIMIT: 
+            countdown = int((start_time + TIME_LIMIT)) - int(time.time())
+            timer = "{:02d}".format(countdown)
+            print(timer, end="\r")
+
+            ret, frame = cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            data[0] = normalized_image
+            prediction = model.predict(data)
+            cv2.imshow('frame', frame)
+            
+            self.user_choice = self.user_choices[prediction.argmax()]
+            
+            # Press q to close the window
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # After the loop release the cap object
+        cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
+        
+        print(f"You have played {self.user_choice}")
+        return self.user_choice
+
+    
+    def get_winner(self):
+
+        self.get_user_choice()
+        self.get_computer_choice()
+        
+        if self.user_choice == self.computer_choice:
+            
+            print("Draw")
+
+        elif (self.user_choice == "rock" and self.computer_choice == "scissors") or (self.user_choice == "scissors" and self.computer_choice == "paper") or (self.user_choice == "paper" and self.computer_choice == "rock"):
+            
+            self.user_wins += 1
+            print("You win this round")
+
+        else:
+
+            self.computer_wins += 1
+            print("Computer wins this round")
+
+        print(f"Your points: {self.user_wins}")
+        print(f"Computer points: {self.computer_wins}")
+
+
+    def play(self):
+
+        while (self.user_wins < self.points_to_win) and (self.computer_wins < self.points_to_win):
+            self.get_winner()
+        
+        if self.computer_wins > self.user_wins:
+            
+            print("You lose")
+
+        else:
+
+            print("You win")
+
+
+
+rps = RockPaperScissors()
+
+if __name__ == "__main__":
+
+    rps.play()
--- test.py
+++ test.py
@@ -1,65 +1,76 @@
-import cv2
-import time
-from keras.models import load_model
-import numpy as np
-import random
+# Almost everything in python is an object, strings, list, floats.
 
-def run():    
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    start_time = time.time()
-    TIME_LIMIT = 4
-    while time.time() < start_time + TIME_LIMIT: 
-        countdown = int((start_time + TIME_LIMIT)) - int(time.time())
-        timer = "{:02d}".format(countdown)
-        print(timer, end="\r")
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        cv2.imshow('frame', frame)
-        options = ["Rock", "Paper", "Scissors", "Nothing"]
-        user_selection = options[prediction.argmax()]
+# When a variable is assigned to an onject. Variable is now an 'instance of that object'
 
-        def get_computer_choice():
-            OPTIONS = ["Rock", "Paper", "Scissors"]
-            computer_choice = random.choice(OPTIONS)
-            print(f"computer plays {computer_choice}")
-            return computer_choice 
+# Objects have attributes that can be accessed via "." list.append() allows us to add to end of list. The '.append' is an attribute
 
-        def get_prediction():
-            print(f"You play {user_selection}")
-            return user_selection
+# Related functions can be grouped into a class. A function inside a class is known as a method
 
-        def get_winner(prediction, computer_choice):
-            if prediction == computer_choice:
-                print("draw")
-                return "draw"
-            elif (prediction == "Rock" and computer_choice == "Scissors") or (prediction == "Scissors" and computer_choice == "Paper") or (prediction == "Paper" and computer_choice == "Rock"):
-                print("you win")
-                return "user"
-            else:
-                print("computer wins")
-                return "computer"
+spongebob = ['SpongeBob SquarePants', 'Fry Cook', 'Sea Sponge', 1500]
+squidward = ['Squidward Tentacles', 'Cashier', 'Octopus', 700]
+krabs = ['Eugene Krabs', 'Owner', 'Crab', 200000]
+patrick = ['Patrick Star', 'Starfish', 0]
 
-        def play():
-            prediction = get_prediction()
-            computer_choice = get_computer_choice()
-            get_winner(prediction, computer_choice)
-        play()
+# Above code is better in a class. As it is difficult to remember what each index refers to in an instance of that object. 
+# The object being the list and the name being the 'instance of that object'
+ 
+def employee(name, position, species, salary):
+    print({'Name' : name, 'Position': position, 'Species': species, 'Salary': salary})
 
-        # Press q to close the window
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
+spongebob = employee('SpongeBob SquarePants', 'Fry Cook', 'Sea Sponge', 1500)
+squidward = employee('Squidward Tentacles', 'Cashier', 'Octopus', 700)
+krabs = employee('Eugene Krabs', 'Owner', 'Crab', 200000)
+patrick = employee('Patrick Star', None, 'Starfish', 0)
 
-    # After the loop release the cap object
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
-    return user_selection
+class ClassName:
+    def __init__(self, param1, param2=1):
+        self.param1 = param1
+        self.param2 = param2
 
-run()
+        self.param3 = param2 * 2
+
+        self.param4 = 0
+
+    def some_method(self, ext_input):
+        return self.param1 + ext_input + ClassName.att
+
+    def some_other_method(self, ext_input1, ext_input2):
+        self.param4 = ext_input1 + ext_input2 # Changes the value of param4
+
+class Employee:
+    def __init__(self, name, position, species, total_hours, salary):
+        self.name = name
+        self.position = position
+        self.species = species
+        self.total_hours = total_hours
+        self.salary = salary
+
+    def increase_salary(self, amount):
+        self.salary += amount
+        self.promotion()
+
+    def work(self):
+        self.total_hours += 1
+        if self.total_hours % 40 == 0:
+            self.increase_salary(50)
+
+    def promotion(self):
+        if self.salary > 200000:
+            self.position = "Co-owner"
+
+spongebob = Employee(name='SpongeBob SquarePants', position='Fry Cook', species='Sea Sponge', total_hours=0, salary=1500)
+
+# If spongebob works 8 hours a day, 5 days a week, and 40 weeks a year
+# what would his salary be at the end of the year?
+
+for _ in range(8 * 5 * 40):
+    spongebob.work()
+
+print(spongebob.salary)
+
+# How many hours does SpongeBob need to work to be promoted to co-owner?
+
+while spongebob.position != "Co-owner":
+    spongebob.work()
+
+print(spongebob.total_hours)
